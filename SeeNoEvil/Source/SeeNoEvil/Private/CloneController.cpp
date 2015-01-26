8fa4e83ab38fe0f5b03d99a859eda72988475147
Matthew <majwang@ucsc.edu>
2015-01-25 20:45:03 -0800

    Added in new classes for clones.

---
+++ SeeNoEvil/Source/SeeNoEvil/Private/CloneController.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SeeNoEvil.h"
+#include "CloneController.h"
+#include "CloneCharacter.h"
+#include "SeeNoEvilCharacter.h"
+ACloneCharacter MyPawn = GetWorld()->SpawnActor<ACloneCharacter>(ACloneCharacter);
+
+
---
+++ SeeNoEvil/Source/SeeNoEvil/Public/CloneController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "CloneController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEENOEVIL_API ACloneController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
